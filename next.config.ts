0a03638cd0b8ac7daca9a4e593c557ec2de0e231
ta-yamada <ta-yamada@gmofh.com>
2024-12-30 13:18:19 +0900

    basePath設定

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: '/taisei',
+  assetPrefix: '/taisei/',
 };
 
 export default nextConfig;
